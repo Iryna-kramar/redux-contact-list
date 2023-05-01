32803d0873825b417e6a9d6225d0a3800e9d0b77
Iryna Kramar <irynakramar@Irynas-MacBook-Air.local>
2023-05-01 11:51:44 +0300

    Display Added Contacts on The Lists Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ContactForm from './components/ContactForm';
+import ContactsList from './components/ContactsList';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <div className="main-container">
+      <Routes>
+        <Route path="/" element={<ContactForm />} />
+        <Route path="/list" element={<ContactsList />}  />
+      </Routes>
     </div>
-  );
-}
+  </BrowserRouter>
+);
 
 export default App;
